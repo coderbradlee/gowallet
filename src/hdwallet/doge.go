8baa176f03f08fae62f077060b9a2e6f749f9452
lzxm160 <lzxm160@sina.com>
2019-01-21 11:17:53 +0800

    test gowallet

--- src/hdwallet/doge.go
+++ src/hdwallet/doge.go
@@ -4,26 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"math/big"
-
-	"encoding/binary"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/btcjson"
-	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
-	"github.com/btcsuite/btcutil/base58"
 	"github.com/btcsuite/btcutil/hdkeychain"
-	"github.com/debian-go/golang-github-nebulouslabs-entropy-mnemonics"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
@@ -31,6 +17,9 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 	"golang.org/x/crypto/scrypt"
 	"hdwallet/nuls"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 )
 
 func GetDOGEBalanceByAddr(address string) (balance string, err error) {
