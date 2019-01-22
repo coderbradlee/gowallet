e3d86fa017de6722fa6ffda440bff9547f342c34
lzxm160 <lzxm160@sina.com>
2019-01-22 12:02:40 +0800

    change english word

--- src/hdwallet/accounts_test.go
+++ src/hdwallet/accounts_test.go
@@ -122,6 +122,7 @@ func TestImport(t *testing.T) {
 		//         Mnemonic=much vacant moral dumb marble now require radio later there broccoli vapor
 		//         Keystore={"crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"d1b4e65859efefbafd013d9753035107"},"ciphertext":"5b972418517d00b76d83a944c908b9a3830e8b45e5eef73cf2dd4b447371b89b","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"80d7356a1ff1e96666b861095cd41a74ffde6eaf12e61813a87117a3c3b5eeea"},"mac":"d4e83c53de6680b9ca9040ca00dc1a5f8e87a4fe21599d4fcf6dc59e21532a1b"},"id":"d5ee4f05-de82-4628-b2ad-fff08bd84b1d","version":3,"address":"80051e53b70d4701ad5bfbed69f6dc09c38aeba9"}
 		encryptedmk, err := CreateWalletByMnnicAndPwd("much vacant moral dumb marble now require radio later there broccoli vapor", "123password")
+
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -135,6 +136,7 @@ func TestImport(t *testing.T) {
 		fmt.Println("addr", addr)
 	}
 	{
+		// betray rate axis sea mechanic awful fiction trim found puzzle ill consider learn habit piece walnut reflect act
 		masterKeyWithmnemonic, err := CreateWalletByteRandAndPwd("test", "test")
 		if err != nil {
 			fmt.Println(err)
