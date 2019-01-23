25ffc2d7bd205b9f0c6468d008262d93a2b75818
lzxm160 <lzxm160@sina.com>
2019-01-23 14:05:30 +0800

    change english word

--- src/testmain/gowallet.go
+++ src/testmain/gowallet.go
@@ -59,7 +59,7 @@ func test4() {
 	fmt.Println("addr", addr)
 }
 func test3() {
-	key, err := wallet.NewKeyFromMnemonic(mnemonic, TypeEther, bip32.FirstHardenedChild, 0, 0)
+	key, err := wallet.NewKeyFromMnemonic(mnemonic, wallet.TypeEther, bip32.FirstHardenedChild, 0, 0)
 	if err != nil {
 		fmt.Println(err)
 		return
