2c92a43324766e5d1be705781f1e214874648a92
lzxm160 <lzxm160@sina.com>
2019-02-10 19:20:48 +0800

    test shift

--- src/testmain/gowallet.go
+++ src/testmain/gowallet.go
@@ -111,8 +111,8 @@ func merges(array []int) (ret []int) {
 	return
 }
 func mergeSort(left []int, right []int) (ret []int) {
-	fmt.Println(left)
-	fmt.Println(right)
+	// fmt.Println(left)
+	// fmt.Println(right)
 	leftLength := len(left)
 	rightLength := len(right)
 	if leftLength < 1 || rightLength < 1 {
