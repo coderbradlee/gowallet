3c7cf2f362e74dfeac96c17352af195dd7e3a3eb
lzxm160 <lzxm160@sina.com>
2019-02-12 12:44:04 +0800

    test shift

--- src/testmain/test.go
+++ src/testmain/test.go
@@ -41,19 +41,22 @@ var (
 )
 
 func test() {
-	intChan := make(chan int, 1)
-	// 一秒后关闭通道。
-	time.AfterFunc(time.Second, func() {
-		close(intChan)
-	})
-	select {
-	case _, ok := <-intChan:
-		if !ok {
-			fmt.Println("The candidate case is closed.")
-			break
-		}
-		fmt.Println("The candidate case is selected.")
-	}
+	var channels = chan int{nil}
+	channels<-1
+	
+	// intChan := make(chan int, 1)
+	// // 一秒后关闭通道。
+	// time.AfterFunc(time.Second, func() {
+	// 	close(intChan)
+	// })
+	// select {
+	// case _, ok := <-intChan:
+	// 	if !ok {
+	// 		fmt.Println("The candidate case is closed.")
+	// 		break
+	// 	}
+	// 	fmt.Println("The candidate case is selected.")
+	// }
 
 	// 准备好几个通道。
 	// intChannels := [3]chan int{
