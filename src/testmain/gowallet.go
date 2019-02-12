dbaa0aee79c4c9595ece92f1bafda527fc1587f3
lzxm160 <lzxm160@sina.com>
2019-02-12 08:56:51 +0800

    test shift

--- src/testmain/gowallet.go
+++ src/testmain/gowallet.go
@@ -12,32 +12,32 @@ var counter = 0
 
 // var Testvar = "main"
 
-type sliceValue []string
+// type sliceValue []string
 
-//new一个存放命令行参数值的slice
-func newSliceValue(vals []string, p *[]string) *sliceValue {
-	*p = vals
-	return (*sliceValue)(p)
-}
+// //new一个存放命令行参数值的slice
+// func newSliceValue(vals []string, p *[]string) *sliceValue {
+// 	*p = vals
+// 	return (*sliceValue)(p)
+// }
 
-/*
-Value接口：
-type Value interface {
-    String() string
-    Set(string) error
-}
-实现flag包中的Value接口，将命令行接收到的值用,分隔存到slice里
-*/
-func (s *sliceValue) Set(val string) error {
-	*s = sliceValue(strings.Split(val, ","))
-	return nil
-}
+// /*
+// Value接口：
+// type Value interface {
+//     String() string
+//     Set(string) error
+// }
+// 实现flag包中的Value接口，将命令行接收到的值用,分隔存到slice里
+// */
+// func (s *sliceValue) Set(val string) error {
+// 	*s = sliceValue(strings.Split(val, ","))
+// 	return nil
+// }
 
-//flag为slice的默认值default is me,和return返回值没有关系
-func (s *sliceValue) String() string {
-	*s = sliceValue(strings.Split("default is me", ","))
-	return "It's none of my business"
-}
+// //flag为slice的默认值default is me,和return返回值没有关系
+// func (s *sliceValue) String() string {
+// 	*s = sliceValue(strings.Split("default is me", ","))
+// 	return "It's none of my business"
+// }
 
 /*
 可执行文件名 -slice="java,go"  最后将输出[java,go]
