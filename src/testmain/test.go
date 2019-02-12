35ba86c0ed024000c6e4b8d292a8ef665de26dec
lzxm160 <lzxm160@sina.com>
2019-02-12 11:21:17 +0800

    test shift

--- src/testmain/test.go
+++ src/testmain/test.go
@@ -46,19 +46,19 @@ func test() {
 	ch1 <- 2 // 通道已满，因此这里会造成阻塞。
 	fmt.Println("---")
 	// 示例2。
-	ch2 := make(chan int, 1)
-	elem, ok := <-ch2 // 通道已空，因此这里会造成阻塞。
-	fmt.Println("---")
-	//_, _ = elem, ok
-	ch2 <- 1
-	fmt.Println("---")
-	// 示例3。
-	var ch3 chan int
-	ch3 <- 1 // 通道的值为nil，因此这里会造成永久的阻塞！
-	fmt.Println("---")
-	<-ch3 // 通道的值为nil，因此这里会造成永久的阻塞！
-	fmt.Println("---")
-	_ = ch3
+	// ch2 := make(chan int, 1)
+	// elem, ok := <-ch2 // 通道已空，因此这里会造成阻塞。
+	// fmt.Println("---")
+	// //_, _ = elem, ok
+	// ch2 <- 1
+	// fmt.Println("---")
+	// // 示例3。
+	// var ch3 chan int
+	// ch3 <- 1 // 通道的值为nil，因此这里会造成永久的阻塞！
+	// fmt.Println("---")
+	// <-ch3 // 通道的值为nil，因此这里会造成永久的阻塞！
+	// fmt.Println("---")
+	// _ = ch3
 }
 func test3() {
 	{
