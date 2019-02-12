f08444e9556df8579c6ba8c6ebb9c117aa45d89a
lzxm160 <lzxm160@sina.com>
2019-02-12 11:37:17 +0800

    test shift

--- src/testmain/test.go
+++ src/testmain/test.go
@@ -40,6 +40,30 @@ var (
 )
 
 func test() {
+
+	ch1 := make(chan int, 2)
+	// 发送方。
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Printf("Sender: sending element %v...\n", i)
+			ch1 <- i
+		}
+		fmt.Println("Sender: close the channel...")
+		close(ch1)
+	}()
+
+	// 接收方。
+	for {
+		elem, ok := <-ch1
+		if !ok {
+			fmt.Println("Receiver: closed channel")
+			break
+		}
+		fmt.Printf("Receiver: received an element: %v\n", elem)
+	}
+
+	fmt.Println("End.")
+
 	// ch1 := make(chan int, 1)
 	// ch1 <- 1
 	// fmt.Println("---")
