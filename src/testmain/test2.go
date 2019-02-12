3bd1823b2f85f89fd26d44675768e9331bf5b96b
lzxm160 <lzxm160@sina.com>
2019-02-13 07:13:07 +0800

    test shift

--- src/testmain/test2.go
+++ src/testmain/test2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	// "unsafe"
+	"sync/atomic"
 )
 
 type Cat struct {
@@ -49,22 +50,24 @@ func NewCat(name string) Cat {
 }
 
 func test() {
-	num := 1
-	sign := make(chan struct{}, num)
-
-	for i := 0; i < 10; i++ {
-		go func(j int) {
-			fmt.Println(j)
-			sign <- struct{}{}
-		}(i)
+	var count uint32
+	trigger := func(i uint32, fn func()) {
+		for {
+			if n := atomic.LoadUint32(&count); n == i {
+				fn()
+				atomic.AddUint32(&count, 1)
+				break
+			}
+			time.Sleep(time.Nanosecond)
+		}
 	}
-
-	// 办法1。
-	//time.Sleep(time.Millisecond * 500)
-
-	// 办法2。
-	for j := 0; j < 10; j++ {
-		<-sign
+	for i := uint32(0); i < 10; i++ {
+		go func(i uint32) {
+			fn := func() {
+				fmt.Println(i)
+			}
+			trigger(i, fn)
+		}(i)
 	}
 
 	// NewCat("little pig").SetName("monster")
