11b193dcc4f77bcf672ee807fdedb2238c34d1cd
lzxm160 <lzxm160@sina.com>
2019-02-13 07:36:20 +0800

    test shift

--- src/testmain/test2.go
+++ src/testmain/test2.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	// "unsafe"
-	"sync/atomic"
+	// "sync/atomic"
 	// "time"
 )
 
@@ -51,26 +51,39 @@ func NewCat(name string) Cat {
 }
 
 func test() {
-	var count uint32
-	trigger := func(i uint32, fn func()) {
-		for {
-			if n := atomic.LoadUint32(&count); n == i {
-				fn()
-				atomic.AddUint32(&count, 1)
-				break
-			}
-			// time.Sleep(time.Nanosecond)
-		}
+	var chanArray [10]chan struct{}
+	for i := 0; i < 10; i++ {
+		chanArray = make(chan struct{}, 0)
 	}
-	for i := uint32(0); i < 10; i++ {
-		go func(i uint32) {
-			fn := func() {
-				fmt.Println(i)
-			}
-			trigger(i, fn)
+	for i := 0; i < 10; i++ {
+		go func(j int) {
+			chanArray[j] <- struct{}{}
+			fmt.Println(j)
 		}(i)
 	}
-	trigger(10, func() {})
+	for i := 0; i < 10; i++ {
+		<-chanArray[i]
+	}
+	// var count uint32
+	// trigger := func(i uint32, fn func()) {
+	// 	for {
+	// 		if n := atomic.LoadUint32(&count); n == i {
+	// 			fn()
+	// 			atomic.AddUint32(&count, 1)
+	// 			break
+	// 		}
+	// 		// time.Sleep(time.Nanosecond)
+	// 	}
+	// }
+	// for i := uint32(0); i < 10; i++ {
+	// 	go func(i uint32) {
+	// 		fn := func() {
+	// 			fmt.Println(i)
+	// 		}
+	// 		trigger(i, fn)
+	// 	}(i)
+	// }
+	// trigger(10, func() {})
 	// NewCat("little pig").SetName("monster")
 	// dog := Cat{name: "little pig"}
 	// dogP := &dog
