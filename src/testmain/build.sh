f4d65cd58ded582cd3a685339f650a525ff04214
lzxm160 <lzxm160@sina.com>
2019-02-13 11:07:01 +0800

    test shift

--- src/testmain/build.sh
+++ src/testmain/build.sh
@@ -1,5 +1,6 @@
 rm -fr gowallet
 export GOPATH=$GOPATH:`pwd`/../../
 echo $GOPATH
-go build -o gowallet .
-./gowallet -name test
+# go build -o gowallet .
+# ./gowallet -name test
+go test testmain/gotest
