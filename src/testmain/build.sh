c8cdfd9facf363fc0ff77ee20689ea54d2d25bbe
lzxm160 <lzxm160@sina.com>
2019-02-13 15:49:22 +0800

    test shift

--- src/testmain/build.sh
+++ src/testmain/build.sh
@@ -7,4 +7,4 @@ echo $GOPATH
 # go test -v -bench=. -run=^$ -cpu=1,2,4 -count=1 -benchmem -coverprofile=xxxx.out -covermode=count testmain/gotest #-run=^$表示不测试任何功能函数，
 # go tool cover -html=xxxx.out
 
-go test -v -cpu=1,2,4 -count=1 -benchmem -run ^testmain/gotest/TestCC$
+go test -v -test.run testmain/gotest/TestCC
