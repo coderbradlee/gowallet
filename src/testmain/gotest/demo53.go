a19c18d0de9d15bcedb714b3f7be83e838260eb9
lzxm160 <lzxm160@sina.com>
2019-02-13 16:58:35 +0800

    test shift

--- src/testmain/gotest/demo53.go
+++ src/testmain/gotest/demo53.go
@@ -44,30 +44,84 @@ func cond() {
 	var lock sync.RWMutex
 	// sendCond 代表专用于发信的条件变量。
 	sendCond := sync.NewCond(&lock)
-	// recvCond 代表专用于收信的条件变量。
-	recvCond := sync.NewCond(lock.RLocker())
+
+	recvCond := sync.NewCond(&lock)
+
+	// send 代表用于发信的函数。
+	send := func(id, index int) {
+		lock.Lock()
+		for mailbox == 1 {
+			sendCond.Wait()
+		}
+		fmt.Printf("put [%d-%d].\n",
+			id, index)
+		mailbox = 1
+
+		lock.Unlock()
+		recvCond.Broadcast()
+		fmt.Printf("Broadcast [%d-%d].\n",
+			id, index)
+	}
+
+	// recv 代表用于收信的函数。
+	recv := func(id, index int) {
+		lock.Lock()
+		for mailbox == 0 {
+			recvCond.Wait()
+		}
+		fmt.Printf("take [%d-%d].\n",
+			id, index)
+		mailbox = 0
+
+		lock.Unlock()
+		sendCond.Signal() // 确定只会有一个发信的goroutine。
+		fmt.Printf("Signal [%d-%d].\n",
+			id, index)
+	}
 
 	// sign 用于传递演示完成的信号。
 	// sign := make(chan struct{}, 3)
-	max := 5
-	go func(max int) { // 用于发信。
-		// defer func() {
-		// 	sign <- struct{}{}
-		// }()
+	max := 6
+	go func(id, max int) { // 用于发信。
 		for i := 1; i <= max; i++ {
-			// time.Sleep(time.Millisecond * 500)
-			lock.Lock()
-			fmt.Println("put:", i)
-			for mailbox == 1 {
-				sendCond.Wait()
-			}
-			fmt.Printf("[%d]: the mailbox is empty.\n", i)
-			mailbox = 1
-			lock.Unlock()
-			recvCond.Signal()
-			fmt.Printf("[%d]: put 1 mail.\n", i)
+			send(id, i)
 		}
-	}(max)
+	}(0, max)
+	go func(id, max int) { // 用于收信。
+		for j := 1; j <= max; j++ {
+			recv(id, j)
+		}
+	}(1, max/2)
+	go func(id, max int) { // 用于收信。
+		for k := 1; k <= max; k++ {
+			recv(id, k)
+		}
+	}(2, max/2)
+
+	// recvCond 代表专用于收信的条件变量。
+	// recvCond := sync.NewCond(lock.RLocker())
+
+	// // sign 用于传递演示完成的信号。
+	// // sign := make(chan struct{}, 3)
+	// max := 5
+	// go func(max int) { // 用于发信。
+	// 	// defer func() {
+	// 	// 	sign <- struct{}{}
+	// 	// }()
+	// 	for i := 1; i <= max; i++ {
+	// 		// time.Sleep(time.Millisecond * 500)
+	// 		lock.Lock()
+	// 		fmt.Println("put:", i)
+	// 		for mailbox == 1 {
+	// 			sendCond.Wait()
+	// 		}
+	// 		fmt.Printf("[%d]: the mailbox is empty.\n", i)
+	// 		mailbox = 1
+	// 		lock.Unlock()
+	// 		recvCond.Signal()
+	// 		fmt.Printf("[%d]: put 1 mail.\n", i)
+	// 	}
+	// }(max)
 	// go func(max int) { // 用于发信。
 	// 	// defer func() {
 	// 	// 	sign <- struct{}{}
