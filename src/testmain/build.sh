19ab444f972b36da281550df74f710b8b4356435
lzxm160 <lzxm160@sina.com>
2019-02-13 11:30:26 +0800

    test shift

--- src/testmain/build.sh
+++ src/testmain/build.sh
@@ -4,4 +4,4 @@ echo $GOPATH
 # go build -o gowallet .
 # ./gowallet -name test
 go test -v testmain/gotest
-go test -v -bench=. -run=^$ testmain/gotest #-run=^$表示不测试任何功能函数，
+go test -v -bench=. -run=^$ -cpu=1 testmain/gotest #-run=^$表示不测试任何功能函数，
