0d8dad6b4165ca9d6d601c88bcb6d60ed8f4bc84
lzxm160 <lzxm160@sina.com>
2019-02-13 12:01:58 +0800

    test shift

--- src/testmain/gotest/demo53_test.go
+++ src/testmain/gotest/demo53_test.go
@@ -53,7 +53,7 @@ func TestFail(t *testing.T) {
 func BenchmarkGetPrimes(b *testing.B) {
 	b.StopTimer()
 	time.Sleep(time.Millisecond * 500) // 模拟某个耗时但与被测程序关系不大的操作。
-	max := 10000
+	max := 1000
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		GetPrimes(max)
