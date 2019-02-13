772910503d182df9ce4538709187ad4320f02420
lzxm160 <lzxm160@sina.com>
2019-02-13 12:18:29 +0800

    test shift

--- src/testmain/build.sh
+++ src/testmain/build.sh
@@ -5,3 +5,4 @@ echo $GOPATH
 # ./gowallet -name test
 # go test -v -cpu=1,2,4 -count=1 -benchmem -coverprofile=xxxx.out -covermode=count testmain/gotest
 go test -v -bench=. -run=^$ -cpu=1,2,4 -count=1 -benchmem -coverprofile=xxxx.out -covermode=count testmain/gotest #-run=^$表示不测试任何功能函数，
+go tool cover -html=xxxx.out
