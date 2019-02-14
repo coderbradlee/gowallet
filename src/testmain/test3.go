a292c2d281c34bc48e8c80581423a28ea80d1721
lzxm160 <lzxm160@sina.com>
2019-02-14 17:56:55 +0800

    test shift

--- src/testmain/test3.go
+++ src/testmain/test3.go
@@ -16,30 +16,67 @@ import (
 )
 
 func test3forstrings() {
-	var builder1 strings.Builder
-	builder1.WriteString("A Builder is used to efficiently build a string using Write methods.")
-	fmt.Printf("The first output(%d):\n%q\n", builder1.Len(), builder1.String())
-	fmt.Println()
-	builder1.WriteByte(' ')
-	builder1.WriteString("It minimizes memory copying. The zero value is ready to use.")
-	builder1.Write([]byte{'\n', '\n'})
-	builder1.WriteString("Do not copy a non-zero Builder.")
-	fmt.Printf("The second output(%d):\n\"%s\"\n", builder1.Len(), builder1.String())
+
+	reader1 := strings.NewReader(
+		"NewReader returns a new Reader reading from s. " +
+			"It is similar to bytes.NewBufferString but more efficient and read-only.")
+	fmt.Printf("The size of reader: %d\n", reader1.Size())
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+
+	buf1 := make([]byte, 47)
+	n, _ := reader1.Read(buf1)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
 	fmt.Println()
 
 	// 示例2。
-	fmt.Println("Grow the builder ...")
-	builder1.Grow(10)
-	fmt.Printf("The length of contents in the builder is %d.\n", builder1.Len())
+	buf2 := make([]byte, 21)
+	offset1 := int64(64)
+	n, _ = reader1.ReadAt(buf2, offset1)
+	fmt.Printf("%d bytes were read. (call ReadAt, offset: %d)\n", n, offset1)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
 	fmt.Println()
-	b := &builder1
-	b.WriteString("================================")
-	fmt.Printf("b(%d):\n\"%s\"\n", b.Len(), b.String())
 
 	// 示例3。
-	fmt.Println("Reset the builder ...")
-	builder1.Reset()
-	fmt.Printf("The third output(%d):\n%q\n", builder1.Len(), builder1.String())
+	offset2 := int64(17)
+	expectedIndex := reader1.Size() - int64(reader1.Len()) + offset2
+	fmt.Printf("Seek with offset %d and whence %d ...\n", offset2, io.SeekCurrent)
+	readingIndex, _ := reader1.Seek(offset2, io.SeekCurrent)
+	fmt.Printf("The reading index in reader: %d (returned by Seek)\n", readingIndex)
+	fmt.Printf("The reading index in reader: %d (computed by me)\n", expectedIndex)
+
+	n, _ = reader1.Read(buf2)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+
+	// var builder1 strings.Builder
+	// builder1.WriteString("A Builder is used to efficiently build a string using Write methods.")
+	// fmt.Printf("The first output(%d):\n%q\n", builder1.Len(), builder1.String())
+	// fmt.Println()
+	// builder1.WriteByte(' ')
+	// builder1.WriteString("It minimizes memory copying. The zero value is ready to use.")
+	// builder1.Write([]byte{'\n', '\n'})
+	// builder1.WriteString("Do not copy a non-zero Builder.")
+	// fmt.Printf("The second output(%d):\n\"%s\"\n", builder1.Len(), builder1.String())
+	// fmt.Println()
+
+	// // 示例2。
+	// fmt.Println("Grow the builder ...")
+	// builder1.Grow(10)
+	// fmt.Printf("The length of contents in the builder is %d.\n", builder1.Len())
+	// fmt.Println()
+	// b := &builder1
+	// b.WriteString("================================")
+	// fmt.Printf("b(%d):\n\"%s\"\n", b.Len(), b.String())
+
+	// // 示例3。
+	// fmt.Println("Reset the builder ...")
+	// builder1.Reset()
+	// fmt.Printf("The third output(%d):\n%q\n", builder1.Len(), builder1.String())
 
 }
 func test3syncpool() {
