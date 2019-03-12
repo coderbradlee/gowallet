f151f9ceba7f5cfd21d50dbfc6e9ceb0eb660a90
lzxm160 <lzxm160@sina.com>
2019-03-12 18:00:56 +0800

    x

--- src/testmain/build.sh
+++ src/testmain/build.sh
@@ -1,14 +1,14 @@
 rm -fr gowallet
 export GOPATH=$GOPATH:`pwd`/../../
 echo $GOPATH
-# go build -o gowallet .
+go build -o gowallet .
 # ./gowallet
 # ./gowallet -name test
 # go test -v -cpu=1,2,4 -count=1 -benchmem -coverprofile=xxxx.out -covermode=count testmain/gotest
 # go test -v -bench=. -run=^$ -cpu=1,2,4 -count=1 -benchmem -coverprofile=xxxx.out -covermode=count testmain/gotest #-run=^$表示不测试任何功能函数，
 # go tool cover -html=xxxx.out
 
-go test -v -test.run TestAll2 ./gotest
+#go test -v -test.run TestAll2 ./gotest
 
 # /demo53_test.go ./gotest/demo53.go
 #go tool vet -shadow gowallet.go
