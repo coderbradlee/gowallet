e2721e6bf920cea3eb7a3f362e78d1618963cd75
lzxm160 <lzxm160@sina.com>
2019-08-01 20:46:50 +0800

    x

--- test/main_test.go
+++ test/main_test.go
@@ -3,45 +3,40 @@ package test
 import (
 	//"runtime"
 
-	"fmt"
-	"io/ioutil"
 	"os"
-	"strconv"
-	"strings"
-
 	//"os"
 	"testing"
 )
 
-func readDir(dir, model string) (uint64, error) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return 0, err
-	}
-	maxN := uint64(0)
-	for _, file := range files {
-		name := file.Name()
-		lens := len(name)
-		if lens < 11 || !strings.Contains(name, model) {
-			continue
-		}
-		num := name[lens-11 : lens-3]
-		n, err := strconv.Atoi(num)
-		if err != nil {
-			continue
-		}
-		if uint64(n) > maxN {
-			maxN = uint64(n)
-		}
-	}
-	return maxN + 1, nil
-}
-func Test(t *testing.T) {
-	x, err := readDir(".", "chain")
-	fmt.Println(x, ":", err)
-	name := "chain" + fmt.Sprintf("-%08d", x) + ".db"
-	fmt.Println(name)
-}
+//func readDir(dir, model string) (uint64, error) {
+//	files, err := ioutil.ReadDir(dir)
+//	if err != nil {
+//		return 0, err
+//	}
+//	maxN := uint64(0)
+//	for _, file := range files {
+//		name := file.Name()
+//		lens := len(name)
+//		if lens < 11 || !strings.Contains(name, model) {
+//			continue
+//		}
+//		num := name[lens-11 : lens-3]
+//		n, err := strconv.Atoi(num)
+//		if err != nil {
+//			continue
+//		}
+//		if uint64(n) > maxN {
+//			maxN = uint64(n)
+//		}
+//	}
+//	return maxN + 1, nil
+//}
+//func Test(t *testing.T) {
+//	x, err := readDir(".", "chain")
+//	fmt.Println(x, ":", err)
+//	name := "chain" + fmt.Sprintf("-%08d", x) + ".db"
+//	fmt.Println(name)
+//}
 func BenchmarkOsStat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		f, _ := os.Stat("chain.db")
