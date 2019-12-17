fe89d62c352425d84367d01733ace02c5fac0724
lzxm160 <lzxm160@sina.com>
2019-12-17 20:21:49 +0800

    x

--- src/hdwallet/ltc.go
+++ src/hdwallet/ltc.go
@@ -339,7 +339,7 @@ func GetLTCBalanceByAddr(address string) (balance string, err error) {
 	if err != nil {
 		return
 	}
-	//fmt.Println("The get Balance info is ", string(bs))
+	fmt.Println("LTC The get Balance info is ", string(bs))
 	err = json.Unmarshal(bs, &rest)
 	if err != nil {
 		//fmt.Println("There are some errors:", err)
