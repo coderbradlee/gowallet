43f785bdd1306681a68f836b6afb1f958c7d6dde
lzxm160 <lzxm160@sina.com>
2020-02-17 12:10:03 +0800

    x

--- main.go
+++ main.go
@@ -6,28 +6,64 @@ import (
 	"time"
 )
 
+//func f() {
+////	container := make([]int, 8)
+////	log.Println("> loop.")
+////	// slice会动态扩容，用它来做堆内存的申请
+////	for i := 0; i < 32*1000*1000; i++ {
+////		container = append(container, i)
+////	}
+////	log.Println("< loop.")
+////	// container在f函数执行完毕后不再使用
+////}
+////func testfmt() {
+////	log.Println("start.")
+////	f()
+////
+////	log.Println("force gc.")
+////	runtime.GC() // 调用强制gc函数
+////
+////	log.Println("done.")
+////	time.Sleep(1 * time.Hour) // 保持程序不退出
+////}
+func traceMemStats() {
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	log.Printf("Alloc:%d(bytes) HeapIdle:%d(bytes) HeapReleased:%d(bytes)", ms.Alloc, ms.HeapIdle, ms.HeapReleased)
+}
+
 func f() {
 	container := make([]int, 8)
 	log.Println("> loop.")
-	// slice会动态扩容，用它来做堆内存的申请
 	for i := 0; i < 32*1000*1000; i++ {
 		container = append(container, i)
+		if i == 16*1000*1000 {
+			traceMemStats()
+
+		}
 	}
 	log.Println("< loop.")
-	// container在f函数执行完毕后不再使用
-}
-func testfmt() {
-	log.Println("start.")
-	f()
-
-	log.Println("force gc.")
-	runtime.GC() // 调用强制gc函数
-
-	log.Println("done.")
-	time.Sleep(1 * time.Hour) // 保持程序不退出
 }
 func main() {
-	testfmt()
+	log.Println("start.")
+	traceMemStats()
+	f()
+	traceMemStats()
+
+	log.Println("force gc.")
+	runtime.GC()
+
+	log.Println("done.")
+	traceMemStats()
+
+	go func() {
+		for {
+			traceMemStats()
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	time.Sleep(1 * time.Hour)
 }
 
 //
